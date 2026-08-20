d2149dc6e8473dd7141e75a7fe548e1464701385
RafalSprengel <sprengel.rafal@gmail.com>
2026-08-20 23:27:24 +0100

    changes

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export function proxy(request: NextRequest) {
-    const sessionCookie = getSessionCookie(request);
+    const sessionCookie = getSessionCookie(request, {
+        cookiePrefix: "__Secure-better-auth",
+    });
 
     if (!sessionCookie) {
         return NextResponse.redirect(new URL("/sign-in", request.url));
